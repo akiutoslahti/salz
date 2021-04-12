76c795a2ce7563bffa82b4da1e763e3de6889947
Aki Utoslahti <aki.utoslahti@helsinki.fi>
2021-04-12 15:38:39 +0300

    Remove bench_block target
    Since libsais is introduced as an alternative to libdivsufsort, bench
    target option is removed to reduce complexity. bench_block program will
    now instead default to benchmarking libsais, libdivsufsort, kkp2 and
    kkp3 during same run.

--- programs/bench_block.c
+++ programs/bench_block.c
@@ -16,40 +16,32 @@
 #include "common.h"
 #include "salz.h"
 #include "divsufsort.h"
+#include "libsais.h"
 
-static size_t kkp2_aux_len(size_t block_len)
-{
-    return block_len + 1;
-}
-
-static size_t kkp3_aux_len(size_t block_len)
-{
-    return 2 * block_len;
-}
+#define NS_IN_SEC 1000000000
 
 int
 main(int argc, const char *argv[])
 {
+    const char *fname;
     FILE *fp;
     char *p;
     long arg_bs;
     size_t log2_min_bs;
     size_t log2_max_bs;
-    char *target_name;
-    int (*target_func)(uint8_t *, size_t, saidx_t *, size_t, int32_t *, size_t);
-    size_t (*target_aux_len_func)(size_t);
     int rc = 0;
 
-    if (argc != 5) {
+    if (argc < 3 || argc > 4) {
         fprintf(stderr, "Invalid arguments\n\n"
-                "Usage: %s [file] [log2_min_bs] [log2_max_bs] [target]\n"
+                "Usage: %s [file] [log2_min_bs] [log2_max_bs]\n"
                 "    file           Path to test file\n"
                 "    log2_min_bs    Log2 of minimum block size\n"
-                "    log2_max_bs    Log2 of maximum block size\n"
-                "    target         Benchmark target: 'kkp2/KKP2' or 'kkp3/KKP3'\n",
+                "    log2_max_bs    Log2 of maximum block size (opt)\n",
                 argv[0]);
         return 1;
     }
+
+    fname = argv[1];
 
     errno = 0;
     arg_bs = strtol(argv[2], &p, 10);
@@ -59,12 +51,16 @@ main(int argc, const char *argv[])
     }
     log2_min_bs = arg_bs;
 
-    arg_bs = strtol(argv[3], &p, 10);
-    if (errno != 0 || *p != '\0' || arg_bs < 0) {
-        fprintf(stderr, "Could not parse maximum block size\n");
-        return 1;
+    if (argc == 4) {
+        arg_bs = strtol(argv[3], &p, 10);
+        if (errno != 0 || *p != '\0' || arg_bs < 0) {
+            fprintf(stderr, "Could not parse maximum block size\n");
+            return 1;
+        }
+        log2_max_bs = arg_bs;
+    } else {
+        log2_max_bs = log2_min_bs;
     }
-    log2_max_bs = arg_bs;
 
     if (log2_min_bs < 10 || log2_min_bs > log2_max_bs || log2_max_bs > 31) {
         fprintf(stderr, "Invalid range for block size - specify log2 sizes in "
@@ -72,27 +68,14 @@ main(int argc, const char *argv[])
         return 1;
     }
 
-    if (strcmp(argv[4], "kkp2") == 0 || strcmp(argv[4], "KKP2") == 0) {
-        target_name = "KKP2";
-        target_func = &kkp2_factor;
-        target_aux_len_func = &kkp2_aux_len;
-    } else if (strcmp(argv[4], "kkp3") == 0 || strcmp(argv[4], "KKP3") == 0) {
-        target_name = "KKP3";
-        target_func = &kkp3_factor;
-        target_aux_len_func = &kkp3_aux_len;
-    } else {
-        fprintf(stderr, "Unknown target: %s\n", argv[4]);
-        return 1;
-    }
-
-    if ((fp = fopen(argv[1], "r")) == NULL) {
+    if ((fp = fopen(fname, "r")) == NULL) {
         perror("fopen");
         return 1;
     }
 
-    printf("block size (log2),block size (b),divsufsort time (s),"
-           "%s time (s),I/O time (s),total time (s),phrases (#)\n",
-           target_name);
+    printf("filename,block size (log2),block size (b),io time (s),"
+           "divsufsort time (s),sais time (s),kkp2 time (s),kkp3 time (s),"
+           "phrases (nr)\n");
 
     for (size_t log2_bs = log2_min_bs; log2_bs <= log2_max_bs; log2_bs++) {
         uint8_t *block;
@@ -102,16 +85,24 @@ main(int argc, const char *argv[])
         int32_t *aux;
         size_t aux_len;
         size_t bytes_read;
-        uint64_t total_ns;
         uint64_t start_ns;
-        uint64_t sa_ns = 0;
-        uint64_t target_ns = 0;
-        size_t nr_target_factors = 0;
+        uint64_t io_ns = 0;
+        uint64_t divsufsort_ns = 0;
+        uint64_t sais_ns = 0;
+        uint64_t kkp2_ns = 0;
+        uint64_t kkp3_ns = 0;
+        size_t nr_kkp2_factors = 0;
+        size_t nr_kkp3_factors = 0;
         int res;
 
         block_len = 1L << log2_bs;
         sa_len = block_len + 2;
-        aux_len = (*target_aux_len_func)(block_len);
+        /*
+         * Auxiliary space demand for text of length N:
+         *   - KKP2 - N + 1
+         *   - KKP3 - 2N
+         */
+        aux_len = 2 * block_len;
 
         block = malloc(block_len * sizeof(*block));
         sa = malloc(sa_len * sizeof(*sa));
@@ -123,11 +114,19 @@ main(int argc, const char *argv[])
             break;
         }
 
-        total_ns = get_time_ns();
-        while ((bytes_read = fread(block, sizeof(*block), block_len, fp)) != 0) {
+        while (1) {
+            /* File I/O */
+            start_ns = get_time_ns();
+            bytes_read = fread(block, sizeof(*block), block_len, fp);
+            io_ns += get_time_ns() - start_ns;
+
+            if (bytes_read == 0)
+                break;
+
+            /* divsufsort */
             start_ns = get_time_ns();
             res = divsufsort((sauchar_t *)block, sa + 1, bytes_read);
-            sa_ns += get_time_ns() - start_ns;
+            divsufsort_ns += get_time_ns() - start_ns;
 
             if (res != 0) {
                 fprintf(stderr, "divsufsort failed\n");
@@ -135,19 +134,43 @@ main(int argc, const char *argv[])
                 break;
             }
 
+            /* kkp2 factorization */
             start_ns = get_time_ns();
-            res = (*target_func)(block, bytes_read, sa, sa_len, aux, aux_len);
-            target_ns += get_time_ns() - start_ns;
+            res = kkp2_factor(block, bytes_read, sa, sa_len, aux, aux_len);
+            kkp2_ns += get_time_ns() - start_ns;
 
             if (res < 0) {
-                fprintf(stderr, "%s factorization failed\n", target_name);
+                fprintf(stderr, "kkp2 factorization failed\n");
                 rc = 1;
                 break;
             }
 
-            nr_target_factors += res;
+            nr_kkp2_factors += res;
+
+            /* sais */
+            start_ns = get_time_ns();
+            res = libsais(block, sa + 1, bytes_read, 0);
+            sais_ns += get_time_ns() - start_ns;
+
+            if (res != 0) {
+                fprintf(stderr, "sais failed\n");
+                rc = 1;
+                break;
+            }
+
+            /* kkp3 factorization */
+            start_ns = get_time_ns();
+            res = kkp3_factor(block, bytes_read, sa, sa_len, aux, aux_len);
+            kkp3_ns += get_time_ns() - start_ns;
+
+            if (res < 0) {
+                fprintf(stderr, "kkp3 factorization failed\n");
+                rc = 1;
+                break;
+            }
+
+            nr_kkp3_factors += res;
         }
-        total_ns = get_time_ns() - total_ns;
 
         if (ferror(fp) || !feof(fp)) {
             fprintf(stderr, "Error or EOF not reached\n");
@@ -160,14 +183,24 @@ main(int argc, const char *argv[])
             rc = 1;
         }
 
-        printf("%zu,%ld,%.5Lf,%.5Lf,%.5Lf,%.5Lf,%zu\n",
+        if (nr_kkp2_factors != nr_kkp3_factors) {
+            fprintf(stderr, "Differing factor counts for kkp2 and kkp3 - "
+                    "kkp2: %ld, kkp3: %ld\n",
+                    nr_kkp2_factors,
+                    nr_kkp3_factors);
+            rc = 1;
+        }
+
+        printf("%s,%zu,%ld,%.5Lf,%.5Lf,%.5Lf,%.5Lf,%.5Lf,%zu\n",
+               fname,
                log2_bs,
                block_len,
-               1.0L * sa_ns / 1000000000,
-               1.0L * target_ns / 1000000000,
-               1.0L * (total_ns - sa_ns - target_ns) / 1000000000,
-               1.0L * total_ns / 1000000000,
-               nr_target_factors);
+               1.0L * io_ns / NS_IN_SEC,
+               1.0L * divsufsort_ns / NS_IN_SEC,
+               1.0L * sais_ns / NS_IN_SEC,
+               1.0L * kkp2_ns / NS_IN_SEC,
+               1.0L * kkp3_ns / NS_IN_SEC,
+               nr_kkp2_factors);
 
         free(block);
         free(sa);
